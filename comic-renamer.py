0838f88042fcecf03520abfe4aa38a266c01c024
nightconcept <dark@nightconcept.net>
2022-02-11 11:46:00 -0800

    Create comic-renamer.py
    Initial commit

---
+++ comic-renamer.py
@@ -0,0 +1,17 @@
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("path", help="path that will be renamed")
+args = parser.parse_args()
+print(args.path)
+
+# parse out path to iterate
+
+# prompt for name
+
+# replace spaces and invalid characters
+
+# iterate through all files
+
+# categorize each into list of old name, volume
+
+# rename each file into new name
